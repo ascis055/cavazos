22aead706d8e1873d8c5570f3ef00704497a3c4c
Alexandra Stankovich <stankovichalexandra@gmail.com>
2024-10-27 21:28:06 -0700

    Initial commit

---
+++ src/main/java/com/github/ascis055/cavazos/CavazosApp.java
@@ -0,0 +1,11 @@
+package com.github.ascis055.cavazos;
+
+/*
+ * Test application
+ */
+public class CavazosApp {
+
+  public static void main(String[] args) {
+      System.out.println("Hello world");
+  }
+}
